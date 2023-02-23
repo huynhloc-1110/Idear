fa9317139f02141cac3782e13cfd0b26d2b7d4a5
huy26062002 <nonemu211@gmail.com>
2023-02-23 08:39:49 +0700

    add category and topic models

---
+++ Idear/Models/Category.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.Build.Framework;
+
+namespace Idear.Models
+{
+    public class Category
+    {
+        public int Id { get; set; }
+        [Required]
+        public string Name { get; set; }
+    }
+}
---
+++ Idear/Models/Topic.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.Build.Framework;
+
+namespace Idear.Models
+{
+    public class Topic
+    {
+        public string Id { get; set; }
+        [Required]
+        public string Name { get; set; }
+        [Required]
+        public DateTime ClosureDate { get; set; }
+        [Required]
+        public DateTime FinalClosureTime { get; set; }
+    }
+}
