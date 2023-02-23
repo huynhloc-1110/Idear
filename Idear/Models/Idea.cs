af15b5736e3fc2ea28538751ad9fd865d086db80
DKHOA111 <dkhoa0120@gmail.com>
2023-02-23 09:19:06 +0700

    Add Idea and react erd

---
+++ Idear/Models/Idea.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using System.ComponentModel.DataAnnotations;
+
+namespace Idear.Models
+{
+	public class Idea
+	{
+		public ApplicationUser? User { get; set; }
+		public Topic? Topic { get; set; }
+
+		public Category? Category { get; set; }
+
+
+		public string? Id { get; set; }
+		[Required]
+		[StringLength(500)]
+		public string? Text { get; set; }
+		public string? FilePath { get; set; }
+
+		[DataType(DataType.Date)]
+		public DateTime DateTime { get; set; }
+
+	}
+}
