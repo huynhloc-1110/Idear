70a26f05e2379fc8ec90226d23dae7e7fd8a6cc6
huynhloc-1110 <101441531+huynhloc-1110@users.noreply.github.com>
2023-02-23 20:54:12 +0700

    Implement simple SeedData
    Add some categories to the database

---
+++ Idear/Data/SeedData.cs
@@ -0,0 +1,39 @@
+﻿using Idear.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Idear.Data
+{
+    public class SeedData
+    {
+        public static void Initialize(IServiceProvider serviceProvider)
+        {
+            using (var context = new ApplicationDbContext(
+                serviceProvider.GetRequiredService<DbContextOptions<ApplicationDbContext>>()))
+            {
+                if (context.Categories.Any())
+                {
+                    return;
+                }
+
+                context.Categories.AddRange(
+                    new Category
+                    {
+                        Id = Guid.NewGuid().ToString(),
+                        Name = "General",
+                    },
+                    new Category
+                    {
+                        Id = Guid.NewGuid().ToString(),
+                        Name = "Workspace",
+                    },
+                    new Category
+                    {
+                        Id = Guid.NewGuid().ToString(),
+                        Name = "Technical",
+                    }
+                );
+                context.SaveChanges();
+            }
+        }
+    }
+}
--- Idear/Program.cs
+++ Idear/Program.cs
@@ -19,17 +19,23 @@ builder.Services.AddControllersWithViews();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+using (var scope = app.Services.CreateScope())
 {
-    app.UseMigrationsEndPoint();
+    var services = scope.ServiceProvider;
+    SeedData.Initialize(services);
 }
-else
-{
-    app.UseExceptionHandler("/Home/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-    app.UseHsts();
-}
+
+    // Configure the HTTP request pipeline.
+    if (app.Environment.IsDevelopment())
+    {
+        app.UseMigrationsEndPoint();
+    }
+    else
+    {
+        app.UseExceptionHandler("/Home/Error");
+        // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+        app.UseHsts();
+    }
 
 app.UseHttpsRedirection();
 app.UseStaticFiles();
