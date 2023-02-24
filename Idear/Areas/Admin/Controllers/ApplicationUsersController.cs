e4a7d28ebfa4be1c97f40d1ea28282984a452f27
DKHOA111 <dkhoa0120@gmail.com>
2023-02-24 12:43:31 +0700

    List Seend Data to View
    Create ApplicationUsersController

---
+++ Idear/Areas/Admin/Controllers/ApplicationUsersController.cs
@@ -0,0 +1,28 @@
+﻿using Idear.Data;
+using Idear.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Idear.Areas.Admin.Controllers
+{
+	[Area("Admin")]
+	public class ApplicationUsersController : Controller
+	{
+		private readonly ApplicationDbContext _context;
+		private readonly RoleManager<IdentityRole> _roleManager;
+		private readonly UserManager<ApplicationUser> _userManager;
+
+		public ApplicationUsersController(ApplicationDbContext context, UserManager<ApplicationUser> userManager, RoleManager<IdentityRole> roleManager)
+		{
+			_userManager = userManager;
+			_roleManager = roleManager;
+			_context = context;
+		}
+
+		//ListAllUser
+		public IActionResult Index()
+		{
+			return View(_context.ApplicationUsers.ToList());
+		}
+	}
+}
