51c4e603daa504aa3fbefff4c462d8d9a9df2e27
DKHOA111 <dkhoa0120@gmail.com>
2023-02-25 20:54:07 +0700

    AddDefaultTokenProvider

--- Idear/Program.cs
+++ Idear/Program.cs
@@ -11,7 +11,7 @@ builder.Services.AddDbContext<ApplicationDbContext>(options =>
     options.UseSqlServer(connectionString));
 builder.Services.AddDatabaseDeveloperPageExceptionFilter();
 
-builder.Services.AddDefaultIdentity<ApplicationUser>()
+builder.Services.AddDefaultIdentity<ApplicationUser>().AddDefaultTokenProviders()
     .AddRoles<IdentityRole>()
     .AddUserManager<UserManager<ApplicationUser>>()
     .AddEntityFrameworkStores<ApplicationDbContext>();
