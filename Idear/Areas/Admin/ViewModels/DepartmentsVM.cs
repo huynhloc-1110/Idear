3443d54e980c92be9445d097b4a44fdc6453ba1c
Erizos <erizospvp@gmail.com>
2023-03-05 13:40:41 +0700

    Added ajax modal on department CRUD

---
+++ Idear/Areas/Admin/ViewModels/DepartmentsVM.cs
@@ -0,0 +1,10 @@
+﻿using Idear.Models;
+
+namespace Idear.Areas.Admin.ViewModels
+{
+    public class DepartmentsVM
+    {
+        public Department? Department { get; set; }
+        public List<Department>? Departments { get; set; }
+    }
+}
