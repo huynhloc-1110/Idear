836626c21c8d53644b23a737b562004e5ea6b0ea
huynhloc-1110 <101441531+huynhloc-1110@users.noreply.github.com>
2023-03-17 16:43:29 +0700

    Implement client validation for AgreeTerms
    Do it by implement IClientModelValidator for IsTrueAttribute and add istrue method to the unobstrutive validator script.

--- Idear/Areas/Staff/ViewModels/CreateIdeasVM.cs
+++ Idear/Areas/Staff/ViewModels/CreateIdeasVM.cs
@@ -1,7 +1,7 @@
 ﻿using Idear.Models;
+using Microsoft.AspNetCore.Mvc.ModelBinding.Validation;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using System.ComponentModel.DataAnnotations;
-using System.Xml.Linq;
 
 namespace Idear.Areas.Staff.ViewModels
 {
@@ -27,24 +27,34 @@ namespace Idear.Areas.Staff.ViewModels
         [Required]
         public bool IsAnonymous { get; set; }
 
-
-
 		[Required]
-		[IsTrue(ErrorMessage ="Please agree to Terms and Conditions")]        
+		[IsTrue(ErrorMessage = "Please agree to Terms and Conditions")]        
 		public bool AgreeTerms { get; set; }
 	}
 
-	public class IsTrueAttribute : ValidationAttribute
+	public class IsTrueAttribute : ValidationAttribute, IClientModelValidator
 	{
-		/// <summary>
-		/// Determines whether the specified value of the object is valid. 
-		/// </summary>
-		/// <returns>
-		/// true if the specified value is valid; otherwise, false. 
-		/// </returns>
-		/// <param name="value">The value of the specified validation object on which the <see cref="T:System.ComponentModel.DataAnnotations.ValidationAttribute"/> is declared.
-		///	</param>
-		public override bool IsValid(object? value)
+        public void AddValidation(ClientModelValidationContext context)
+        {
+            MergeAttribute(context.Attributes, "data-val", "true");
+            MergeAttribute(context.Attributes, "data-val-istrue", GetErrorMessage());
+        }
+
+        private static string GetErrorMessage()
+            => "Please agree to Terms and Conditions";
+
+        private static bool MergeAttribute(IDictionary<string, string> attributes, string key, string value)
+        {
+            if (attributes.ContainsKey(key))
+            {
+                return false;
+            }
+
+            attributes.Add(key, value);
+            return true;
+        }
+
+        public override bool IsValid(object? value)
 		{
 			if (value == null) return false;
 			if (value.GetType() != typeof(bool)) throw new InvalidOperationException("can only be used on boolean properties.");
