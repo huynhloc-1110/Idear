b39036fc1e5d82c5782387d326b44bc700004dd3
huynhloc-1110 <101441531+huynhloc-1110@users.noreply.github.com>
2023-04-01 22:29:44 +0700

    Implement hide and status mark for report
    -Hide and Mark action are put in QAManager/ReportsController
    - Use switch expression instead of switch case in StatisticsController
    - Implement ajax for hide and status mark action
    - Fix frontend problem for _Layout, which is a wrong class for dropdown-item

---
+++ Idear/Areas/QAManager/Controllers/ReportsController.cs
@@ -0,0 +1,63 @@
+﻿using Idear.Data;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace Idear.Areas.QAManager.Controllers
+{
+    [Area("QAManager")]
+    [Authorize(Roles = "QA Manager")]
+    public class ReportsController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+        
+        public ReportsController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpPut]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Mark(string id, int status)
+        {
+            var report = await _context.Reports
+                .FindAsync(id);
+            if (report == null)
+            {
+                return BadRequest("Report not found");
+            }
+
+            report.Status = (Models.ReportStatus)status;
+            await _context.SaveChangesAsync();
+
+            return Ok();
+        }
+
+        [HttpPut]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> ToggleHide(string id)
+        {
+            var report = await _context.Reports
+                .Include(r => r.ReportedIdea)
+                .Include(r => r.ReportedComment)
+                .FirstOrDefaultAsync(r => r.Id == id);
+            if (report == null)
+            {
+                return BadRequest("Report not found");
+            }
+
+            // toggle is hidden
+            if (report.ReportedIdea != null)
+            {
+                report.ReportedIdea.IsHidden = !report.ReportedIdea.IsHidden;
+            }
+            if (report.ReportedComment != null)
+            {
+                report.ReportedComment.IsHidden = !report.ReportedComment.IsHidden;
+            }
+            await _context.SaveChangesAsync();
+
+            return Ok();
+        }
+    }
+}
